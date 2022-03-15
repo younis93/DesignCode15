7e707b7d4b00ea1fd7a36c3f836b93983f68a473
Younis <younis.only2@gmail.om>
2022-03-15 21:25:06 +0300

    Initial Commit

---
+++ DesignCode15/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DesignCode15
+//
+//  Created by Younis J on 15/03/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DesignCode15/DesignCode15App.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCode15App.swift
+//  DesignCode15
+//
+//  Created by Younis J on 15/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct DesignCode15App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
