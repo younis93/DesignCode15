5619cf7dd12f8e7c44cde620835102c8520ff366
Younis <younis.only2@gmail.om>
2022-03-21 14:34:32 +0300

    3- Layout Code and Preview

--- DesignCode15/ContentView.swift
+++ DesignCode15/ContentView.swift
@@ -17,57 +17,49 @@ struct ContentView: View {
                 .aspectRatio(contentMode: .fit)
                 .frame(width: 26.0, height: 26.0)
                 .cornerRadius(20)
+                .padding(5)
+                .background(Color("Background"))
+                .cornerRadius(30)
 
             Text("SwiftUI for IOS 15")
                 .font(.largeTitle)
                 .fontWeight(.bold)
             
-            Text("20 sections 3 hours")
+            Text("20 sections 3 hours".uppercased())
                 .font(.footnote)
                 .fontWeight(.semibold)
+                .foregroundColor(.secondary)
+
+            
             Text("Build an iOS app for iOS 15 with custom layouts,animations and ...")
                 .font(.footnote)
                 .lineLimit(2)
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .foregroundColor(.secondary)
         }
-        .padding(20.0)
+        .padding(.all, 20.0)
+        .padding(.vertical, 20)
         .frame(height: 350.0)
-        .background(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color("Background")/*@END_MENU_TOKEN@*/)
+        .background(Color("Background"))
         .cornerRadius(30.0)
-        .shadow(radius: /*@START_MENU_TOKEN@*/20/*@END_MENU_TOKEN@*/)
-        
-        VStack (alignment: .leading, spacing: 8){
-            Spacer()
-            
-            Image("Logo 2")
-                .resizable()
-                .scaledToFit()
-                .cornerRadius(20)
-                .frame(width: 26, height: 26)
-            
-            
-            Text("SwiftUI for IOS 15")
-                .font(.largeTitle)
-                .bold()
-            
-            Text("20 sections 3 hours")
-                .font(.footnote)
-                .fontWeight(.semibold)
-            
-            Text("Build an iOS app for iOS 15 with custom layouts,animations and ...")
-                .lineLimit(2)
-                .font(.footnote)
-        }
-        .padding(20)
-        .frame(height: 350.0)
-        .background(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color("Background")/*@END_MENU_TOKEN@*/)
-        .cornerRadius(30)
-        .shadow(radius: 30)
+        .shadow(color: Color("Shadow").opacity(0.3), radius: 10, x: 0, y: 10)
+        .padding(.horizontal,20)
     }
     
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
+        Group {
+            ContentView()
+            
+            ContentView()
+                .preferredColorScheme(.dark)
+                .previewLayout(.device)
+        }
+            
         ContentView()
+            .previewDevice("iPad Pro (11-inch) (3rd generation)")
+
     }
 }
