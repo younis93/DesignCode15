bc02d9763837c3f44008ad11b9917f60d8b4f25f
Younis <younis.only2@gmail.om>
2022-03-26 15:22:01 +0300

    8  SF Symbols 3

--- DesignCode15/AccountView.swift
+++ DesignCode15/AccountView.swift
@@ -11,10 +11,41 @@ struct AccountView: View {
     var body: some View {
         NavigationView {
             List{
+                VStack(spacing: 8) {
+                    Image(systemName: "person.crop.circle.badge.checkmark")
+                        .symbolVariant(.circle.fill)
+                        .font(.system(size: 32))
+                        .symbolRenderingMode(.palette)
+                        .foregroundStyle(.blue, .blue.opacity(0.3))
+                        .padding()
+                        .background(Circle().fill(.ultraThinMaterial))
+                        .background(
+                            Image(systemName: "hexagon")
+                                .symbolVariant(.fill)
+                                .foregroundColor(.blue)
+                                .font(.system(size: 200))
+                                .offset(x: -50, y: -100)
+                        )
+                    
+                    Text("Younis J")
+                        .font(.title .weight(.semibold))
+                    
+                    HStack {
+                        Image(systemName: "location")
+                            .imageScale(.small)
+                        
+                        Text("Iraq")
+                            .foregroundColor(.secondary)
+                    }
+                    
+                }
+                .frame(maxWidth: .infinity)
+                .padding()
+                
                 Section {
-                    Text("Setting")
-                    Text("Billing")
-                    Text("Help")
+                    Label("Setting", systemImage: "gear")
+                    Label("Billing", systemImage: "creditcard")
+                    Label("Help", systemImage: "questionmark")
                 }
                 .listRowSeparatorTint(.blue)
                 .listRowSeparator(.hidden)
@@ -24,13 +55,6 @@ struct AccountView: View {
         }
     }
 }
-
-
-
-
-
-
-
 
 
 struct AccountView_Previews: PreviewProvider {
