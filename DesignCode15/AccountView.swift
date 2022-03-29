6f74aadeae1a7ba3abf680d831006b142da4eab4
Younis <younis.only2@gmail.om>
2022-03-29 22:12:40 +0300

    10  States and Swipe Actions

--- DesignCode15/AccountView.swift
+++ DesignCode15/AccountView.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 struct AccountView: View {
+    @State var isDeleted: Bool = false
+    
     var body: some View {
         NavigationView {
             List{
@@ -60,12 +62,25 @@ struct AccountView: View {
                 .listRowSeparator(.hidden)
                 
                 Section {
-                    Link(destination: URL(string: "https://google.com")!) {
-                        HStack {
-                            Label("Website", systemImage: "house")
-                            Spacer()
-                            Image(systemName: "link")
-                                .foregroundColor(.secondary)
+                    if !isDeleted {
+                        Link(destination: URL(string: "https://google.com")!) {
+                            HStack {
+                                Label("Website ", systemImage: "house")
+                                Spacer()
+                                Image(systemName: "link")
+                                    .foregroundColor(.secondary)
+                            }
+                        }
+                        .swipeActions(edge: .leading, allowsFullSwipe: true) {
+                            Button (action: {isDeleted = true}) {
+                                Label("Delete", systemImage: "trash")
+                            }
+                            .tint(.red)
+                            
+                            Button {} label: {
+                                Label("Pin", systemImage: "pin")
+                            }
+                            .tint(.yellow)
                         }
                     }
                     
