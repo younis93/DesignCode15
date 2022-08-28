13e82904e89aa656bf1c944d713747c3941399a4
Younis <younis.only2@gmail.om>
2022-08-28 21:26:11 +0300

    28. Parallax and Rotation 3D Effect

--- DesignCode15/Views/HomeView.swift
+++ DesignCode15/Views/HomeView.swift
@@ -51,15 +51,31 @@ struct HomeView: View {
     
     var featured : some View {
         TabView {
-            ForEach(courses) { item in
-                FeaturedItem(course: item)
+            ForEach(courses) { course in
+                GeometryReader { proxy in
+                    let minX = proxy.frame(in: .global).minX
+                    FeaturedItem(course: course)
+                        .padding(.vertical, 40)
+                        .rotation3DEffect(.degrees(minX / -10), axis: (x: 0, y: 1, z: 0))
+                        .shadow(color: Color("Shadow").opacity(0.3), radius: 10, x: 0, y: 10)
+                        .blur(radius: abs(minX / 40))
+                        .overlay(
+                            Image(course.image)
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(height: 230)
+                                .offset(x: 32, y: -80)
+                                .offset(x: minX / 2)
+                        )
+//                    Text("\(proxy.frame(in: .global).minX)")
+                }
             }
         }
         .tabViewStyle(.page(indexDisplayMode: .never))
         .frame(height: 430)
         .background(
             Image("Blob 1")
-                .offset(x: 250, y: -100)
+                .offset(x: 250, y: -60)
         )
     }
 }
@@ -67,6 +83,7 @@ struct HomeView: View {
 struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
         HomeView()
+            .preferredColorScheme(.dark)
     }
 }
 
