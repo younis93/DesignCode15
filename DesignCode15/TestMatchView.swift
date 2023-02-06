cab42ed89476a503709027955ecb14a070deb0aa
Younis <younis.only2@gmail.om>
2023-02-06 19:52:22 +0300

    30. Transition Between Views

--- DesignCode15/TestMatchView.swift
+++ DesignCode15/TestMatchView.swift
@@ -1,21 +1,21 @@
 //
-//  MatchView.swift
+//  TestMatchView.swift
 //  DesignCode15
 //
-//  Created by Younis J on 09/01/2023.
+//  Created by Younis J on 21/01/2023.
 //
 
 import SwiftUI
 
-struct MatchView: View {
-    @Namespace var namespace
+struct TestMatchView: View {
     @State var show = false
+    @Namespace var namespace
     var body: some View {
-        ZStack {
+        ZStack{
             if !show {
                 VStack {
                     Spacer()
-                    VStack(alignment: .leading, spacing: 12){
+                    VStack (alignment: .leading, spacing: 12){
                         Text("SwiftUI")
                             .font(.largeTitle.weight(.bold))
                             .matchedGeometryEffect(id: "title", in: namespace)
@@ -28,7 +28,6 @@ struct MatchView: View {
                             .matchedGeometryEffect(id: "text", in: namespace)
                     }
                     .padding(20)
-                    .foregroundStyle(.white)
                     .background(
                         Rectangle()
                             .fill(.ultraThinMaterial)
@@ -37,6 +36,7 @@ struct MatchView: View {
                             .matchedGeometryEffect(id: "blur", in: namespace)
                     )
                 }
+                .foregroundStyle(.white)
                 .background(
                     Image("Illustration 9")
                         .resizable()
@@ -44,9 +44,9 @@ struct MatchView: View {
                         .matchedGeometryEffect(id: "image", in: namespace)
                 )
                 .background(
-                    Image("Background 5")
+                    Image("Background 9")
                         .resizable()
-                        .aspectRatio(contentMode: .fill)
+                        .aspectRatio(contentMode: .fit)
                         .matchedGeometryEffect(id: "background", in: namespace)
                 )
                 .mask(
@@ -55,23 +55,24 @@ struct MatchView: View {
                 )
                 .frame(height: 300)
                 .padding(20)
-            } else {
-                ScrollView() {
+            }else {
+                ScrollView {
                     VStack {
                         Spacer()
                     }
                     .frame(maxWidth: .infinity)
-                    .frame(height: 500)
                     
-                    .foregroundStyle(.primary)
+                    
+                    .frame(height: 500)
                     .background(
                         Image("Illustration 9")
                             .resizable()
                             .aspectRatio(contentMode: .fit)
                             .matchedGeometryEffect(id: "image", in: namespace)
                     )
+                    
                     .background(
-                        Image("Background 5")
+                        Image("Background 9")
                             .resizable()
                             .aspectRatio(contentMode: .fill)
                             .matchedGeometryEffect(id: "background", in: namespace)
@@ -92,9 +93,8 @@ struct MatchView: View {
                             Text("Build an iOS app for iOS 15 with custom layouts, animations and ...")
                                 .font(.footnote)
                                 .matchedGeometryEffect(id: "text", in: namespace)
-                            
                             Divider()
-                            HStack {
+                            HStack{
                                 Image("Avatar Default")
                                     .resizable()
                                     .frame(width: 26, height: 26)
@@ -102,11 +102,11 @@ struct MatchView: View {
                                     .padding(8)
                                     .background(.ultraThinMaterial, in: RoundedRectangle(cornerRadius: 18, style: .continuous))
                                     .strokeStyle(cornerRadius: 18)
-                                Text("Taught by Meng To and Stephanie Diep")
-                                    .font(.footnote)
+                                Text("Taught By mento")
                             }
                         }
                             .padding(20)
+                        
                             .background(
                                 Rectangle()
                                     .fill(.ultraThinMaterial)
@@ -120,19 +120,15 @@ struct MatchView: View {
             }
         }
         .onTapGesture {
-            withAnimation(.spring(response: 0.6, dampingFraction: 0.8)) {
+            withAnimation(.spring(response: 0.6, dampingFraction: 0.9)) {
                 show.toggle()
             }
         }
     }
 }
 
-struct MatchView_Previews: PreviewProvider {
+struct TestMatchView_Previews: PreviewProvider {
     static var previews: some View {
-        MatchView()
+        TestMatchView()
     }
 }
-
-
-
-
